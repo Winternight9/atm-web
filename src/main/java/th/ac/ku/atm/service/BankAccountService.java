353acbcd977da81f9f345e099bfa712189e3d473
Winternight9 <poomiix9@gmail.com>
2020-10-05 14:27:12 +0700

    connect bankaccount api

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -1,15 +1,33 @@
 package th.ac.ku.atm.service;
 
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import th.ac.ku.atm.model.BankAccount;
-import th.ac.ku.atm.model.Customer;
+import org.springframework.web.client.RestTemplate;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
+
 
 @Service
-
 public class BankAccountService {
+
+    private RestTemplate restTemplate;
+
+    public BankAccountService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+    public List<BankAccount> getCustomerBankAccount(int customerId) {
+        String url = "http://localhost:8091/api/bankaccount/customer/" +
+                customerId;
+        ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+
+        BankAccount[] accounts = response.getBody();
+
+        return Arrays.asList(accounts);
+    }
 
     private ArrayList<BankAccount> bankAccountList;
 
